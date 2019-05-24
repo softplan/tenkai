e4f4b44c3d5430ce1d3a0d914c518fd1ce5b82bd
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-05-24 17:32:54 -0300

    Improve Deployment screen

--- tenkai-webapp/src/views/Deployment.jsx
+++ tenkai-webapp/src/views/Deployment.jsx
@@ -3,8 +3,10 @@ import {
   Grid,
   Row,
   Col,
-  Table
+  FormGroup, ControlLabel, Table
 } from "react-bootstrap";
+import Select from 'react-select';
+
 
 import { Card } from "components/Card/Card.jsx";
 import Button from "components/CustomButton/CustomButton.jsx";
@@ -15,82 +17,126 @@ import axios from 'axios';
 class Deployment extends Component {
 
   state = {
-    chartsResult: {charts:[]}
-  }    
-
+    selectedOption: null,
+    chartsResult: { charts: [] },
+    environmentList: []
+  }
+ 
   componentDidMount() {
+    this.getEnvironments();
     this.getCharts()
+  }
+
+  handleEnvironmentChange = (selectedOption) => {
+    this.setState({ selectedOption });
+  }
+
+
+  getEnvironments() {
+    axios.get('http://localhost:8080/environments')
+      .then(response => {
+
+
+        var arr = [];
+        for (var x=0; x < response.data.Envs.length; x++) {
+          var element = response.data.Envs[x];
+          console.log(element);
+          arr.push({ value: element.name, label: element.name });
+        }
+
+        this.setState({ environmentList: arr });
+
+
+      })
+      .catch(error => console.log(error.message))
   }
 
   getCharts() {
     axios.get('http://localhost:8080/charts')
-    .then(response => this.setState({chartsResult: response.data}))
-    .catch(error => console.log(error.message))
+      .then(response => this.setState({ chartsResult: response.data }))
+      .catch(error => console.log(error.message))
   }
 
   render() {
-           
+
+
+    const { selectedOption } = this.state;
+
     const items = this.state.chartsResult.charts.map((item, key) =>
-            
-    <tr key={key} >
-        <td><input type="checkbox" className="checkbox"/></td>
+
+      <tr key={key} >
+        <td><input type="checkbox" className="checkbox" /></td>
         <td>{item.name}</td>
         <td>{item.chartVersion}</td>
         <td>{item.description}</td>
-    </tr>
+      </tr>
 
-  );       
+    );
 
 
     return (
       <div className="content">
         <Grid fluid>
           <Row>
+
             <Col md={12}>
               <Card
-                title=""
+                title="Environment"
                 content={
+                  <div>
 
-                            <Row>
-                                <Col md={12}>
+                    <Row>
+                      <div className="col-md-5">
+                        <FormGroup>
+                          <ControlLabel>Environment</ControlLabel>
+                          <Select value={selectedOption}  onChange={this.handleEnvironmentChange} options={this.state.environmentList} />
+                        </FormGroup>
 
-                                  <Card
-                                      title="Deployment"
-                                      content={
-                                          <form>
-                                              
-                                              <div>
-                                              <Table  bordered hover size="sm">
-                                                  <thead>
-                                                      <tr>
-                                                      <th>#</th>
-                                                      <th>Helm Chart</th>
-                                                      <th>Version</th>
-                                                      <th>Description</th>
-                                                      </tr>
-                                                  </thead>
-                                                  <tbody>
-                                                    {items}
-                                                  </tbody>
-                                                  </Table>
-              
-                                              </div>
+                      </div>
 
-                                              <Button bsStyle="info" fill type="button">Analyse Dependencies</Button>               
+                    </Row>
 
-                                            
-                                              
-                                          </form>
-                                      }
-                                  />
-                              </Col>
-                          </Row>                    
-
+                    <div className="clearfix" />
+                  </div>
                 }
               />
             </Col>
           </Row>
 
+          <Row>
+            <Col md={12}>
+
+              <Card
+                title="Helm Chart"
+                content={
+                  <div>
+
+                    <div>
+                      <Table bordered hover size="sm">
+                        <thead>
+                          <tr>
+                            <th>#</th>
+                            <th>Helm Chart</th>
+                            <th>Version</th>
+                            <th>Description</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {items}
+                        </tbody>
+                      </Table>
+
+                    </div>
+
+                    <Button bsStyle="info" fill type="button">Analyse Dependencies</Button>
+
+
+
+                  </div>
+                }
+              />
+            </Col>
+          </Row>
 
         </Grid>
       </div>
