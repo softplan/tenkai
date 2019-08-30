5a5925c9967d38cbfc1929fc7ce7b62b34f2825d
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-08-30 14:36:58 -0300

    Added Security Features in Promote environment

--- src/components/Modal/EditModal.jsx
+++ src/components/Modal/EditModal.jsx
@@ -4,9 +4,25 @@ import { Modal } from "react-bootstrap";
 class EditModal extends Component {
    
     render() {
+
+        function Header(props) {
+            
+            if (props.props.header) {
+                return (
+                    <Modal.Header closeButton>
+                        <Modal.Title>{props.props.title}</Modal.Title>
+                    </Modal.Header>                    
+                );
+            } else {
+                return (
+                    <div></div>
+                )
+            }
+        }
         
         return (
             <Modal bsSize="large" show={this.props.onShow} onHide={this.props.onClose.bind(this)}>
+                <Header props={this.props}/>
                 <Modal.Body>
                     {this.props.form}
                 </Modal.Body>
--- src/views/Workload.jsx
+++ src/views/Workload.jsx
@@ -9,6 +9,7 @@ import { PodPanel } from 'components/Workload/PodPanel.jsx';
 import { ServicePanel } from 'components/Workload/ServicePanel.jsx';
 import Button from "components/CustomButton/CustomButton.jsx";
 import CopyModal from "components/Modal/CopyModal.jsx";
+import EditModal from "components/Modal/EditModal";
 
 
 class Workload extends Component {
@@ -21,6 +22,9 @@ class Workload extends Component {
         podList: [],
         serviceList: [],
         onShowCopyModal: false,
+        onShowConfirmModal: false,
+        targetEnvToPromote: {},
+        confirmInput: "",
     }
 
     componentDidMount() {
@@ -33,7 +37,7 @@ class Workload extends Component {
             this.listPods();
             this.listDeploymentsByEnv();
             this.listServices();
-        }, 25000);
+        }, 10000);
     }
 
     onCloseCopyModal() {
@@ -41,8 +45,14 @@ class Workload extends Component {
     }
     
     async onConfirmCopyModal(item) {
-        promote(this, this.props.selectedEnvironment.value, item.value);
-        this.setState({ onShowCopyModal: false});
+        this.setState({ onShowCopyModal: false, onShowConfirmModal: true, targetEnvToPromote: item});
+    }
+
+    async doPromote() {
+        if (this.state.confirmInput === this.state.targetEnvToPromote.label) {
+            promote(this, this.props.selectedEnvironment.value, this.state.targetEnvToPromote.value);
+            this.setState({ onShowConfirmModal: false, targetEnvToPromote: {} });
+        }
     }
     
     showConfirmCopyModal(ref) {
@@ -107,6 +117,15 @@ class Workload extends Component {
         })
     }
 
+    onCloseConfirmModal() {
+        this.setState({ onShowConfirmModal: false, targetEnvToPromote: {} });
+    }
+
+    handleConfirmInputChange(e) {
+        let value = e.target.value;
+        this.setState({confirmInput: value});
+    }
+
     
     render() {
 
@@ -125,59 +144,7 @@ class Workload extends Component {
 
         return (
 
-
-
-            <Tabs defaultActiveKey="helm" id="workload-tab" onSelect={this.onTabSelect.bind(this)}>
-                <Tab eventKey="helm" title="Helm Releases">
-                    <Card
-                        title=""
-                        content={
-                            <div>
-
-                                    <CopyModal
-                                        onShow={this.state.onShowCopyModal}
-                                        onClose={this.onCloseCopyModal.bind(this)}
-                                        title="Select target environment (it will be destroyed)" 
-                                        onConfirm={this.onConfirmCopyModal.bind(this)}
-                                        environments={this.props.environments}
-                                        onlyMyEnvironments={true}>
-                                    </CopyModal>                                
-
-
-                                <Row>
-                                    <Col xs={8}>
-                                        <FormGroup>
-                                            <ControlLabel>Release Search</ControlLabel>
-                                            <FormControl
-                                                value={this.state.inputFilter}
-                                                onChange={this.onChangeInputHandler.bind(this)}
-                                                style={{ width: '100%' }} type="text"
-                                                aria-label="Search"></FormControl>
-                                        </FormGroup>
-                                    </Col>
-                                    <Col xs={4}>
-                                    <ButtonToolbar>
-                                        <Button className="btn btn-danger btn-fill pull-right" bsSize="sm" 
-                                            onClick={this.showConfirmCopyModal.bind(this)}><i className="pe-7s-smile" />
-                                        {" "}Copy Releases to another namespace</Button>
-                                    </ButtonToolbar>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col xs={12}>
-
-                                        <PanelGroup accordion id="workload-accordion">
-                                            {items}
-                                        </PanelGroup>
-                                    </Col>
-
-                                </Row>
-                            </div>
-
-                        } />
-
-
-                </Tab>
+            <Tabs defaultActiveKey="pods" id="workload-tab" onSelect={this.onTabSelect.bind(this)}>
                 <Tab eventKey="pods" title="Pods">
                     <Card
                         title=""
@@ -248,6 +215,88 @@ class Workload extends Component {
                         } />
 
                 </Tab>
+
+                <Tab eventKey="helm" title="Helm Releases">
+                    <Card
+                        title=""
+                        content={
+                            <div>
+                                <CopyModal
+                                    onShow={this.state.onShowCopyModal}
+                                    onClose={this.onCloseCopyModal.bind(this)}
+                                    title="Select target environment (it will be destroyed)" 
+                                    onConfirm={this.onConfirmCopyModal.bind(this)}
+                                    environments={this.props.environments}
+                                    onlyMyEnvironments={true}>
+                                </CopyModal>       
+
+                                <EditModal
+                                    title="Confirm"
+                                    header={true}
+                                    onShow={this.state.onShowConfirmModal}
+                                    onClose={this.onCloseConfirmModal.bind(this)}
+                                    form={
+                                        <div>
+                                            <p>This is a <b>dangerous</b> operation, target environment <b> {this.state.targetEnvToPromote.label}  </b> within all variables will be destroyed!</p>
+                                            <p>Please, enter the target environment name to confirm!</p>
+                                            <input text name="confirmInput" value={this.state.confirmInput} onChange={this.handleConfirmInputChange.bind(this)}/>
+                                            <hr/>
+                                            <ButtonToolbar>
+                                                <Button className="btn btn-danger btn-fill" bsSize="sm"  disabled={this.state.confirmInput !== this.state.targetEnvToPromote.label}
+                                                    onClick={this.doPromote.bind(this)}><i className="pe-7s-smile" 
+                                                    />
+                                                {" "}Confirm</Button>
+                                                <Button className="btn btn-info btn-fill" bsSize="sm" 
+                                                    onClick={this.onCloseConfirmModal.bind(this)}><i className="pe-7s-smile" />
+                                                {" "}Cancel</Button>
+
+                                            </ButtonToolbar>
+
+
+                                                
+                                        </div>
+                                    } 
+                                />                                                             
+                                
+
+                                <Row>
+                                    <Col xs={8}>
+                                        <FormGroup>
+                                            <ControlLabel>Release Search</ControlLabel>
+                                            <FormControl
+                                                value={this.state.inputFilter}
+                                                onChange={this.onChangeInputHandler.bind(this)}
+                                                style={{ width: '100%' }} type="text"
+                                                aria-label="Search"></FormControl>
+                                        </FormGroup>
+                                    </Col>
+                                    <Col xs={4}>
+                                    <ButtonToolbar>
+                                        <Button className="btn btn-danger btn-fill pull-right" bsSize="sm" 
+                                            onClick={this.showConfirmCopyModal.bind(this)}
+                                            disabled={!this.props.keycloak.hasRealmRole("tenkai-admin")}>
+                                            <i className="pe-7s-smile" />
+                                        {" "}Copy Releases to another namespace</Button>
+                                    </ButtonToolbar>
+                                    </Col>
+                                </Row>
+                                <Row>
+                                    <Col xs={12}>
+
+                                        <PanelGroup accordion id="workload-accordion">
+                                            {items}
+                                        </PanelGroup>
+                                    </Col>
+
+                                </Row>
+                            </div>
+
+                        } />
+
+
+                </Tab>
+
+
             </Tabs>
 
         );
