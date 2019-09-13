cc7e06a7a0b4cb91ecd1ef2387da4369ded8f484
Denny Richard San Vriesman <dvriesman@gmail.com>
2019-09-13 13:25:09 -0300

    Fix Docker

--- config/config.sh
+++ config/config.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+
+echo $API_URL >> /tmp/variables.txt
+
 if [ -n "$API_URL" ]; then
     find "$1" -type f -name '*.js' -exec sed -i 's~http:\/\/localhost\:8080~'"$API_URL"'~g' {} \;
 fi
+
+echo $KEYCLOAK_URL >> /tmp/variables.txt
+
 if [ -n "$KEYCLOAK_URL" ]; then
-    find "$1" -type f -name 'keycloak.json' -exec sed -i 's~http:\/\/keycloak-tools\/auth~'"$KEYCLOAK_URL"'~g' {} \;
+    find "$1" -type f -name 'keycloak.json' -exec sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' {} \;
 fi
+
+echo $KEYCLOAK_REALM >> /tmp/variables.txt
+
 if [ -n "$KEYCLOAK_REALM" ]; then
     find "$1" -type f -name 'keycloak.json' -exec sed -i 's~tenkai~'"$KEYCLOAK_REALM"'~g' {} \;
 fi
