7df5008637bf57de5ed42c9f6c832bea8cca6606
André Lins <andrelucas01@hotmail.com>
2019-09-24 19:11:18 -0300

    Refactoring in components Card|CustomButton|CustomRadio

--- src/components/Card/ActionCard.jsx
+++ src/components/Card/ActionCard.jsx
@@ -1,65 +1,60 @@
-import React, { Component } from "react";
-import {
-     Row, Col, ButtonToolbar
-  } from "react-bootstrap";
+import React from "react";
+import { Row, Col, ButtonToolbar } from "react-bootstrap";
 import Button from "components/CustomButton/CustomButton.jsx";
-  
 
-export class ActionCard extends Component {
-  render() {
-    return (
-      <div className={"card" + (this.props.plain ? " card-plain" : "")}>
-        <div className={"header" + (this.props.hCenter ? " text-center" : "")} style={{backgroundColor: "#FAFAFA"}}>
-            <Row>
-            <Col md={6}>
-                <h4 className="title">{this.props.title}</h4>
-            </Col>
-            <Col md={6}>
+export const ActionCard = props => (
+  <div className={"card" + (props.plain ? " card-plain" : "")}>
+    <div
+      className={"header" + (props.hCenter ? " text-center" : "")}
+      style={{ backgroundColor: "#FAFAFA" }}
+    >
+      <Row>
+        <Col md={6}>
+          <h4 className="title">{props.title}</h4>
+        </Col>
+        <Col md={6}>
+          <ButtonToolbar>
+            <Button
+              className="btn btn-info pull-right btn-fill"
+              type="button"
+              disabled={props.buttonsDisabled}
+              onClick={props.analyseOnClick}
+            >
+              <i className="pe-7s-eyedropper" /> Analyse
+            </Button>
 
-                    <ButtonToolbar>
-                      <Button className="btn btn-info pull-right btn-fill"
-                        type="button" 
-                        disabled={this.props.buttonsDisabled}
-                        onClick={this.props.analyseOnClick}>
-                        
-                        <i className="pe-7s-eyedropper"/>{" "}Analyse
-                      </Button>
+            <Button
+              className="btn btn-primary pull-right btn-fill"
+              type="button"
+              disabled={props.buttonsDisabled}
+              onClick={props.directDeployOnClick}
+            >
+              <i className="pe-7s-album" /> Deploy
+            </Button>
+          </ButtonToolbar>
+        </Col>
+      </Row>
+    </div>
+    <div
+      className={
+        "content" +
+        (props.ctAllIcons ? " all-icons" : "") +
+        (props.ctTableFullWidth ? " table-full-width" : "") +
+        (props.ctTableResponsive ? " table-responsive" : "") +
+        (props.ctTableUpgrade ? " table-upgrade" : "")
+      }
+    >
+      {props.content}
 
-                      <Button className="btn btn-primary pull-right btn-fill"
-                        type="button"
-                        disabled={this.props.buttonsDisabled}
-                        onClick={this.props.directDeployOnClick}>
-                        
-                        <i className="pe-7s-album"/>{" "}Deploy
-                      </Button>
-                    </ButtonToolbar>
-
-                
-            </Col>
-          </Row>  
-        </div>
-        <div
-          className={
-            "content" +
-            (this.props.ctAllIcons ? " all-icons" : "") +
-            (this.props.ctTableFullWidth ? " table-full-width" : "") +
-            (this.props.ctTableResponsive ? " table-responsive" : "") +
-            (this.props.ctTableUpgrade ? " table-upgrade" : "")
-          }
-        >
-          {this.props.content}
-
-          <div className="footer">
-            {this.props.legend}
-            {this.props.stats != null ? <hr /> : ""}
-            <div className="stats">
-              <i className={this.props.statsIcon} /> {this.props.stats}
-            </div>
-          </div>
+      <div className="footer">
+        {props.legend}
+        {props.stats != null ? <hr /> : ""}
+        <div className="stats">
+          <i className={props.statsIcon} /> {props.stats}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
 export default ActionCard;
--- src/components/Card/ChartCard.jsx
+++ src/components/Card/ChartCard.jsx
@@ -1,50 +1,63 @@
-import React, { Component } from "react";
-import {
-    ButtonToolbar
-} from "react-bootstrap";
+import React from "react";
+import { ButtonToolbar } from "react-bootstrap";
 
 import Button from "components/CustomButton/CustomButton.jsx";
 
-export class ChartCard extends Component {
-  render() {
-    return (
-        <div className={"card chart"}>
+export const ChartCard = props => (
+  <div className={"card chart"}>
+    <div className={"header"}>
+      <ButtonToolbar>
+        <Button
+          className="btn-primary btn-fill"
+          bsSize="sm"
+          onClick={() =>
+            props.deploy(props.item.name + "@" + props.item.chartVersion)
+          }
+        >
+          <i className="pe-7s-album" /> Deploy
+        </Button>
 
-        <div className={"header"}>
+        <Button
+          className="btn-info btn-fill"
+          bsSize="sm"
+          onClick={() =>
+            props.analysis(`${props.item.name}@${props.item.chartVersion}`)
+          }
+        >
+          <i className="pe-7s-eyedropper" /> Analyse
+        </Button>
 
-            <ButtonToolbar>
+        <Button
+          className="btn"
+          bsSize="sm"
+          onClick={() =>
+            props.addToDeployList(
+              `${props.item.name}@${props.item.chartVersion}`
+            )
+          }
+        >
+          <i className="pe-7s-angle-right-circle" /> Multiple deploy
+        </Button>
+      </ButtonToolbar>
+    </div>
 
-              <Button className="btn-primary btn-fill" bsSize="sm"onClick={this.props.deploy.bind(this, this.props.item.name + "@" + this.props.item.chartVersion)}
-                ><i className="pe-7s-album"/>{" "}Deploy
-                    </Button>
-
-              <Button className="btn-info btn-fill" bsSize="sm" onClick={this.props.analysis.bind(this, this.props.item.name + "@" + this.props.item.chartVersion)}
-                ><i className="pe-7s-eyedropper"/>{" "}Analyse
-                    </Button>
-
-              <Button className="btn" bsSize="sm" onClick={this.props.addToDeployList.bind(this, this.props.item.name + "@" + this.props.item.chartVersion)}
-                ><i className="pe-7s-angle-right-circle"/>{" "}Multiple deploy
-                    </Button>
-
-            </ButtonToolbar>
-        </div>
-
-
-
-        <div className={"header"}>
-          
-          <h4 className="title">
-          
-          <b>{this.props.item.name}</b></h4>
-          <p className="category">{this.props.item.chartVersion}</p>
-        </div>
-        <div className={"content"}>
-            <p><i>App Version: </i>{this.props.item.appVersion}</p>
-            <p><i>Description: </i>{this.props.item.description}</p>
-        </div>
-      </div>
-    );
-  }
-}
+    <div className={"header"}>
+      <h4 className="title">
+        <b>{props.item.name}</b>
+      </h4>
+      <p className="category">{props.item.chartVersion}</p>
+    </div>
+    <div className={"content"}>
+      <p>
+        <i>App Version: </i>
+        {props.item.appVersion}
+      </p>
+      <p>
+        <i>Description: </i>
+        {props.item.description}
+      </p>
+    </div>
+  </div>
+);
 
 export default ChartCard;
