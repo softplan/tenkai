e4af83a48c3d1b8ed2bbc22faa7c9be1667e6bf6
André Lins <andrelucas01@hotmail.com>
2019-09-24 19:38:45 -0300

    Refactoring in CustomCheckbox|RepoForm

--- src/components/Config/RepoForm.jsx
+++ src/components/Config/RepoForm.jsx
@@ -1,129 +1,104 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Card } from "components/Card/Card.jsx";
 import { FormInputs } from "components/FormInputs/FormInputs.jsx";
-import {
-    Button
-} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
-export class RepoForm extends Component {
-
-    state = {
-        formData: {
-            name: '',
-            url: '',
-            password: '',
-            username: '',
+export const RepoForm = ({ editMode, editItem, ...props }) => {
+  const [formData, setFormData] = useState(
+    editItem
+      ? editItem
+      : {
+          name: "",
+          url: "",
+          password: "",
+          username: ""
         }
-    }
+  );
 
-    componentDidMount() {
-        if(this.props.editItem) {
-            this.setState(() => ({
-                formData: this.props.editItem
-            }));
-        } else {
-            this.setState(() => ({
-                formData: {}
-            }));
-        }
-    }
+  const handleChange = ({ value, name }) =>
+    setFormData(formData => ({ ...formData, [name]: value }));
 
-    handleChange = event => {
-        const { value, name } = event.target;
-        this.setState(state => ({
-            formData: {
-                ...state.formData,
-                [name]: value
-            }
-        }));
-    }
+  return (
+    <div>
+      <Card
+        title={editMode ? "Edit Repository" : "New Repository"}
+        content={
+          <form>
+            <FormInputs
+              ncols={["col-md-2", "col-md-10"]}
+              properties={[
+                {
+                  name: "name",
+                  label: "Name",
+                  type: "text",
+                  bsClass: "form-control",
+                  value: formData.name,
+                  onChange: e => handleChange(e)
+                },
+                {
+                  name: "url",
+                  label: "URL",
+                  type: "text",
+                  bsClass: "form-control",
+                  placeholder: "https://artifactory/artifactory/xpto-helm",
+                  value: formData.url,
+                  onChange: e => handleChange(e)
+                }
+              ]}
+            />
 
-    saveClick = event => {
-        event.preventDefault();
-        const data = this.state.formData;
-        this.props.saveClick(data);
-    }    
+            <FormInputs
+              ncols={["col-md-3", "col-md-3"]}
+              properties={[
+                {
+                  name: "username",
+                  label: "Username",
+                  type: "text",
+                  bsClass: "form-control",
+                  value: formData.username,
+                  onChange: e => handleChange(e)
+                },
+                {
+                  name: "password",
+                  label: "Password",
+                  type: "password",
+                  bsClass: "form-control",
+                  value: formData.password,
+                  onChange: e => handleChange(e)
+                }
+              ]}
+            />
 
-
-    render() {
-        const { editMode } = this.props;
-
-        return (
-            <div>
-                <Card title={editMode ? "Edit Repository" : "New Repository"}
-                    content={
-                        <form>
-                            <FormInputs
-                                ncols={["col-md-2", "col-md-10"]}
-                                properties={[
-                                    {
-                                        name: "name",
-                                        label: "Name",
-                                        type: "text",
-                                        bsClass: "form-control",
-                                        value: this.state.formData.name,
-                                        onChange: this.handleChange
-                                    },
-                                    {
-                                        name: "url",
-                                        label: "URL",
-                                        type: "text",
-                                        bsClass: "form-control",
-                                        placeholder: "https://artifactory/artifactory/xpto-helm",
-                                        value: this.state.formData.url,
-                                        onChange: this.handleChange
-                                    }
-                                ]}
-                            />
-
-                            <FormInputs
-                                ncols={["col-md-3", "col-md-3"]}
-                                properties={[
-                                    {
-                                        name: "username",
-                                        label: "Username",
-                                        type: "text",
-                                        bsClass: "form-control",
-                                        value: this.state.formData.username,
-                                        onChange: this.handleChange
-
-                                        
-                                    },
-                                    {
-                                        name: "password",
-                                        label: "Password",
-                                        type: "password",
-                                        bsClass: "form-control",
-                                        value: this.state.formData.password,
-                                        onChange: this.handleChange
-
-                                    }
-                                ]}
-                            />                                         
-                         
-                            <div className="btn-toolbar">
-                                <div className="btn-group">
-                                    <Button bsStyle="info" type="button" onClick={this.saveClick}>
-                                        Save
-                                    </Button>
-                                </div>
-                                <div className="btn-group">
-                                    <Button bsStyle="info" type="button" onClick={this.props.cancelClick}>
-                                        Cancel
-                                    </Button>
-
-                                </div>
-                            </div>
-
-                            <div className="clearfix" />
-
-                        </form>
-                    }
-                />
+            <div className="btn-toolbar">
+              <div className="btn-group">
+                <Button
+                  bsStyle="info"
+                  type="button"
+                  onClick={e => {
+                    e.preventDefault();
+                    props.saveClick(formData);
+                  }}
+                >
+                  Save
+                </Button>
+              </div>
+              <div className="btn-group">
+                <Button
+                  bsStyle="info"
+                  type="button"
+                  onClick={props.cancelClick}
+                >
+                  Cancel
+                </Button>
+              </div>
             </div>
-        );
-    }
 
-}
+            <div className="clearfix" />
+          </form>
+        }
+      />
+    </div>
+  );
+};
 
 export default RepoForm;
