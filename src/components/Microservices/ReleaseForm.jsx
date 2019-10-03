cb09c3803f195eafdda0076ce67952e9f1fedeec
André Lins <andrelucas01@hotmail.com>
2019-10-03 07:04:11 -0300

    Set microservices

--- src/components/Microservices/ReleaseForm.jsx
+++ src/components/Microservices/ReleaseForm.jsx
@@ -1,92 +1,76 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Card } from "components/Card/Card.jsx";
 import { FormInputs } from "components/FormInputs/FormInputs.jsx";
-import { Button} from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
+export const ReleaseForm = props => {
+  const [formData, setFormData] = useState({
+    ID: "",
+    chartName: "",
+    release: ""
+  });
 
-export class ReleaseForm extends Component {
-
-    state = {
-        formData: {
-            ID: "",
-            chartName: "",
-            release: "",
-        }
+  useEffect(() => {
+    if (props.editItem) {
+      setFormData(props.editItem);
     }
+  }, []);
 
-    componentDidMount() {
-        if(this.props.editItem) {
-            this.setState(() => ({
-                formData: this.props.editItem
-            }));
-        } else {
-            this.setState(() => ({
-                formData: {
-                    ID: "",
-                    chartName: "",
-                    release: "",
+  return (
+    <div>
+      <Card
+        title={props.editMode ? "Edit Release" : "New Release"}
+        content={
+          <form>
+            <FormInputs
+              ncols={["col-md-2"]}
+              properties={[
+                {
+                  name: "release",
+                  label: "Release Number",
+                  type: "text",
+                  bsClass: "form-control",
+                  placeholder: "Version",
+                  value: formData.release,
+                  onChange: event => {
+                    const { value, name } = event.target;
+                    setFormData(formData => ({
+                      ...formData,
+                      [name]: value
+                    }));
+                  }
                 }
-            }));
-        }
-    }
-
-    handleChange = event => {
-        const { value, name } = event.target;
-        this.setState(state => ({
-            formData: {
-                ...state.formData,
-                [name]: value
-            }
-        }));
-    }
-
-    saveClick = event => {
-        event.preventDefault();
-        const data = this.state.formData;
-        this.props.saveClick(data);
-    }    
-
-    render() {
-        const { editMode } = this.props;
-        return (
-            <div>
-                <Card title={editMode ? "Edit Release" : "New Release"}
-                    content={
-                        <form>
-                            <FormInputs
-                                ncols={["col-md-2"]}
-                                properties={[
-                                    {
-                                        name: "release",
-                                        label: "Release Number",
-                                        type: "text",
-                                        bsClass: "form-control",
-                                        placeholder: "Version",
-                                        value: this.state.formData.release,
-                                        onChange: this.handleChange
-
-                                    }
-                                ]}
-                            />
-                            <div className="btn-toolbar">
-                                <div className="btn-group">
-                                    <Button bsStyle="info" type="button" onClick={this.saveClick}>
-                                        Save
-                                    </Button>
-                                </div>
-                                <div className="btn-group">
-                                    <Button bsStyle="info" type="button" onClick={this.props.cancelClick}>
-                                        Cancel
-                                    </Button>
-                                </div>
-                            </div>
-                            <div className="clearfix" />
-                        </form>
-                    }
-                />
+              ]}
+            />
+            <div className="btn-toolbar">
+              <div className="btn-group">
+                <Button
+                  bsStyle="info"
+                  type="button"
+                  onClick={event => {
+                    event.preventDefault();
+                    props.saveClick(formData);
+                  }}
+                >
+                  Save
+                </Button>
+              </div>
+              <div className="btn-group">
+                <Button
+                  bsStyle="info"
+                  type="button"
+                  onClick={props.cancelClick}
+                >
+                  Cancel
+                </Button>
+              </div>
             </div>
-        );
-    }
-}
+            <div className="clearfix" />
+          </form>
+        }
+      />
+    </div>
+  );
+};
 
 export default ReleaseForm;
