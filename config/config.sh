48e272913ac517011e7d779d9371032079d64204
Anderson Custódio <afcustodioo@gmail.com>
2019-10-16 15:51:05 -0300

    Fixed docker image to resolve variables in some files before run tenkai. (#17)
    * Changed Dockerfile to use entrypoint instead of cmd.
    
    * Changed Dockerfile to use entrypoint instead of cmd.
    
    * Added logs
    
    * Added logs
    
    * Changing find to full path.
    
    * Removing unnecessary file.
    
    * Restoring default travis ci file.

--- config/config.sh
+++ config/config.sh
@@ -1,25 +1,33 @@
 #!/bin/sh
+HTDOCS_FOLDER=$1
 
-echo "$1" >> /tmp/variables.txt
-ls -al $1 >> /tmp/variables.txt
+echo "###### Variables #####"
+echo "HTDOCS_FOLDER $HTDOCS_FOLDER"
+echo "API_URL $API_URL"
+echo "KEYCLOAK_URL $KEYCLOAK_URL"
+echo "KEYCLOAK_REALM $KEYCLOAK_REALM"
+echo "######################"
 
-echo $API_URL >> /tmp/variables.txt
+echo "Before: ${HTDOCS_FOLDER}/keycloak.json"
+cat ${HTDOCS_FOLDER}/keycloak.json
 
 if [ "$API_URL" ]; then
-	  echo "replacing $API_URL" >> /tmp/variables.txt
+    echo find "$1" -type f -name '*.js' -exec sed -i 's~http:\/\/localhost\:8080~'"$API_URL"'~g' {} \;
     find "$1" -type f -name '*.js' -exec sed -i 's~http:\/\/localhost\:8080~'"$API_URL"'~g' {} \;
 fi
 
-echo $KEYCLOAK_URL >> /tmp/variables.txt
-
 if [ "$KEYCLOAK_URL" ]; then
-	  echo "replacing $KEYCLOAK_URL" >> /tmp/variables.txt
-    find "$1" -type f -name 'keycloak.json' -exec sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' {} \;
+    echo sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' ${HTDOCS_FOLDER}/keycloak.json
+    sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' ${HTDOCS_FOLDER}/keycloak.json
 fi
-
-echo $KEYCLOAK_REALM >> /tmp/variables.txt
 
 if [ "$KEYCLOAK_REALM" ]; then
-	  echo "replacing $KEYCLOAK_REALM" >> /tmp/variables.txt
-    find "$1" -type f -name 'keycloak.json' -exec sed -i 's~tenkai~'"$KEYCLOAK_REALM"'~g' {} \;
+    echo sed -i 's~tenkai~'"$KEYCLOAK_REALM"'~g' ${HTDOCS_FOLDER}/keycloak.json
+    sed -i 's~tenkai~'"$KEYCLOAK_REALM"'~g' ${HTDOCS_FOLDER}/keycloak.json
 fi
+
+
+echo "After: ${HTDOCS_FOLDER}/keycloak.json"
+cat ${HTDOCS_FOLDER}/keycloak.json
+
+echo "Up and running!"
