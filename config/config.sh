b13e5cf80cf092e947628d382a2f290f203738f0
Anderson <afcustodioo@gmail.com>
2019-10-16 14:50:36 -0300

    Added logs

--- config/config.sh
+++ config/config.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-ls -al $1 >> /tmp/variables.txt
 echo "###### variaveis #####"
-echo "$1" >> /tmp/variables.txt
-echo "$API_URL" >> /tmp/variables.txt
-echo "$KEYCLOAK_URL" >> /tmp/variables.txt
-echo "$KEYCLOAK_REALM" >> /tmp/variables.txt
+echo "$1"
+echo "$API_URL"
+echo "$KEYCLOAK_URL"
+echo "$KEYCLOAK_REALM"
 echo "######################"
 
 if [ "$API_URL" ]; then
-    echo "replacing $API_URL" >> /tmp/variables.txt
     echo find "$1" -type f -name '*.js' -exec sed -i 's~http:\/\/localhost\:8080~'"$API_URL"'~g' {} \;
     find "$1" -type f -name '*.js' -exec sed -i 's~http:\/\/localhost\:8080~'"$API_URL"'~g' {} \;
 fi
@@ -17,15 +15,12 @@ fi
 echo $KEYCLOAK_URL >> /tmp/variables.txt
 
 if [ "$KEYCLOAK_URL" ]; then
-    echo "replacing $KEYCLOAK_URL" >> /tmp/variables.txt
-    echo find "$1" -type f -name 'keycloak.json' -exec sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' {} \;
-    find "$1" -type f -name 'keycloak.json' -exec sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' {} \;
+    echo sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' /var/www/localhost/htdocs/keycloak.json
+    sed -i 's~http:\/\/keycloaktools\/auth~'"$KEYCLOAK_URL"'~g' /var/www/localhost/htdocs/keycloak.json
+    cat /var/www/localhost/htdocs/keycloak.json
 fi
 
-echo $KEYCLOAK_REALM >> /tmp/variables.txt
-
 if [ "$KEYCLOAK_REALM" ]; then
-    echo "replacing $KEYCLOAK_REALM" >> /tmp/variables.txt
     echo find "$1" -type f -name 'keycloak.json' -exec sed -i 's~tenkai~'"$KEYCLOAK_REALM"'~g' {} \;
     find "$1" -type f -name 'keycloak.json' -exec sed -i 's~tenkai~'"$KEYCLOAK_REALM"'~g' {} \;
 fi
