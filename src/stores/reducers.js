cc03372b4d729485025d8ed087472dc4bfce8ca1
André Lins <andrelucas01@hotmail.com>
2019-11-02 18:36:22 -0300

    Create actionTypes and actions in productReleaseService store

--- src/stores/reducers.js
+++ src/stores/reducers.js
@@ -1,5 +1,6 @@
 import dependency from "./dependency/reducer";
 import product from "./product/reducer";
 import productRelease from "./productRelease/reducer";
+import productReleaseService from "./productReleaseService/reducer";
 
-export { dependency, product, productRelease };
+export { dependency, product, productRelease, productReleaseService };
