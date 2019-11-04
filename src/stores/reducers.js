6167bdfa2c5e93913746f2a0fd56c340d6e1e1b5
André Lins <andrelucas01@hotmail.com>
2019-11-04 20:57:31 -0300

    Create actions in solution store

--- src/stores/reducers.js
+++ src/stores/reducers.js
@@ -2,5 +2,6 @@ import dependency from "./dependency/reducer";
 import product from "./product/reducer";
 import productRelease from "./productRelease/reducer";
 import productReleaseService from "./productReleaseService/reducer";
+import solution from "./solution/reducer";
 
-export { dependency, product, productRelease, productReleaseService };
+export { dependency, product, productRelease, productReleaseService, solution };
