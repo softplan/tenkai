e280a2528d77616a7c66ea9043ea6ac4ff7bcc78
André Lins <andrelucas01@hotmail.com>
2019-11-04 21:25:23 -0300

    Create actions and actionTypes in solution store

--- src/stores/reducers.js
+++ src/stores/reducers.js
@@ -3,5 +3,13 @@ import product from "./product/reducer";
 import productRelease from "./productRelease/reducer";
 import productReleaseService from "./productReleaseService/reducer";
 import solution from "./solution/reducer";
+import solutionChart from "./solutionChart/reducer";
 
-export { dependency, product, productRelease, productReleaseService, solution };
+export {
+  dependency,
+  product,
+  productRelease,
+  productReleaseService,
+  solution,
+  solutionChart
+};
