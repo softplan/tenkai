e336caefdd9a77b4f4cef679c212e0977819ca32
Anderson <afcustodioo@gmail.com>
2020-02-13 17:34:32 -0300

    Add tenkai table in Solution screen.

--- src/views/Solution.jsx
+++ src/views/Solution.jsx
@@ -1,29 +1,31 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import {
   Container,
   Row,
   Col,
   FormControl,
-  Table,
   FormGroup,
   FormLabel,
   ButtonToolbar
-} from "react-bootstrap";
+} from 'react-bootstrap';
 
-import Button from "components/CustomButton/CustomButton.jsx";
-import { CardTenkai } from "components/Card/CardTenkai.jsx";
-import CButton from "components/CustomButton/CustomButton.jsx";
-import SolutionForm from "components/Solution/SolutionForm.jsx";
-import SimpleModal from "components/Modal/SimpleModal.jsx";
+import Button from 'components/CustomButton/CustomButton.jsx';
+import { CardTenkai } from 'components/Card/CardTenkai.jsx';
+import CButton from 'components/CustomButton/CustomButton.jsx';
+import SolutionForm from 'components/Solution/SolutionForm.jsx';
+import SimpleModal from 'components/Modal/SimpleModal.jsx';
 
-import * as solutionActions from "stores/solution/actions";
-import * as solutionSelectors from "stores/solution/reducer";
+import * as solutionActions from 'stores/solution/actions';
+import * as solutionSelectors from 'stores/solution/reducer';
+
+import TenkaiTable from 'components/Table/TenkaiTable';
+import * as col from 'components/Table/TenkaiColumn';
 
 class Solution extends Component {
   state = {
     showInsertUpdateForm: false,
-    inputFilter: "",
+    inputFilter: '',
     showConfirmDeleteModal: false,
     itemToDelete: {}
   };
@@ -54,73 +56,67 @@ class Solution extends Component {
     this.setState({ showConfirmDeleteModal: false, itemToDelete: {} });
   }
 
+  onEdit = item => {
+    this.setState({
+      showInsertUpdateForm: true,
+      editItem: item,
+      editMode: true
+    });
+    window.scrollTo(0, 0);
+  };
+
+  onDelete = item => {
+    this.setState({ itemToDelete: item }, () => {
+      this.setState({ showConfirmDeleteModal: true });
+    });
+  };
+
+  btnCharts = (cell, row) => {
+    return (
+      <Button
+        className="link-button"
+        onClick={() =>
+          this.props.history.push({
+            pathname: '/admin/solution-deps',
+            search: '?solutionId=' + row.ID
+          })
+        }
+      >
+        <i className="pe-7s-album cell-button-icon" />
+      </Button>
+    );
+  };
+
+  btnDeploy = (cell, row) => {
+    return (
+      <Button
+        className="link-button"
+        onClick={() =>
+          this.props.history.push({
+            pathname: '/admin/solution-deploy',
+            search: '?solutionId=' + row.ID
+          })
+        }
+      >
+        <i className="pe-7s-helm" />
+      </Button>
+    );
+  };
+
   render() {
-    const items = this.props.solutions
-      .filter(
-        d =>
-          this.state.inputFilter === "" ||
-          d.name.includes(this.state.inputFilter)
-      )
-      .map((item, key) => (
-        <tr key={key}>
-          <td>{item.ID}</td>
-          <td>{item.name}</td>
-          <td>{item.team}</td>
-          <td>
-            <Button
-              className="link-button"
-              onClick={() => {
-                this.setState(() => ({
-                  showInsertUpdateForm: true,
-                  editItem: item,
-                  editMode: true
-                }));
-                window.scrollTo(0, 0);
-              }}
-            >
-              <i className="pe-7s-edit" />
-            </Button>
-          </td>
-          <td>
-            <Button
-              className="link-button"
-              onClick={() =>
-                this.setState({ itemToDelete: item }, () => {
-                  this.setState({ showConfirmDeleteModal: true });
-                })
-              }
-            >
-              <i className="pe-7s-trash" />
-            </Button>
-          </td>
-          <td>
-            <Button
-              className="link-button"
-              onClick={() =>
-                this.props.history.push({
-                  pathname: "/admin/solution-deps",
-                  search: "?solutionId=" + item.ID
-                })
-              }
-            >
-              <i className="pe-7s-album" />
-            </Button>
-          </td>
-          <td>
-            <Button
-              className="link-button"
-              onClick={() =>
-                this.props.history.push({
-                  pathname: "/admin/solution-deploy",
-                  search: "?solutionId=" + item.ID
-                })
-              }
-            >
-              <i className="pe-7s-helm" />
-            </Button>
-          </td>
-        </tr>
-      ));
+    let columns = [];
+    columns.push(col.addId());
+    columns.push(col.addCol('name', 'Solution Name', '25%'));
+    columns.push(col.addCol('team', 'Team', '25%'));
+    columns.push(col.addEdit(this.onEdit));
+    columns.push(col.addDelete(this.onDelete));
+    columns.push(col.addColBtn('charts', 'Charts', this.btnCharts));
+    columns.push(col.addColBtn('deploy', 'Deploy', this.btnDeploy));
+
+    let data = this.props.solutions.filter(
+      d =>
+        this.state.inputFilter === '' || d.name.includes(this.state.inputFilter)
+    );
 
     return (
       <div className="content">
@@ -196,30 +192,14 @@ class Solution extends Component {
                           onChange={e =>
                             this.setState({ inputFilter: e.target.value })
                           }
-                          style={{ width: "100%" }}
+                          style={{ width: '100%' }}
                           type="text"
                           placeholder="Search using any field"
                           aria-label="Search using any field"
                         ></FormControl>
                       </FormGroup>
                     </div>
-
-                    <div>
-                      <Table bordered hover size="sm">
-                        <thead>
-                          <tr>
-                            <th>#</th>
-                            <th>Solution Name</th>
-                            <th>Team</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                            <th>Charts</th>
-                            <th>Deploy</th>
-                          </tr>
-                        </thead>
-                        <tbody>{items}</tbody>
-                      </Table>
-                    </div>
+                    <TenkaiTable columns={columns} data={data} />
                   </form>
                 }
               />
