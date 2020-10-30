0561444c5cd9cdb0ed18d50fc762edd3a61c42ab
Anderson Fernando Custódio <anderson.custodio@softplan.com.br>
2020-10-30 15:21:45 -0300

    Change Deploy screen to support multi environments deploy coming from Product or Deployment screens.

--- src/stores/deploy/actionTypes.js
+++ src/stores/deploy/actionTypes.js
@@ -1,11 +1,22 @@
 export const LOAD_PRODUCT_CHARTS = 'LOAD_PRODUCT_CHARTS';
+export const LOAD_MULTI_ENV_CHARTS = 'LOAD_MULTI_ENV_CHARTS';
 export const DEPLOY_SUCCESS = 'DEPLOY_SUCCESS';
 export const DEPLOY_ERROR = 'DEPLOY_ERROR';
 
 export const loadProductCharts = data => ({
   type: LOAD_PRODUCT_CHARTS,
   payload: {
+    deployType: 'PRODUCT',
     productVersionId: data.productVersionId,
+    chartsToDeploy: data.chartsToDeploy
+  }
+});
+
+export const loadMultiEnvDeployWithoutProduct = data => ({
+  type: LOAD_MULTI_ENV_CHARTS,
+  payload: {
+    deployType: 'MULTI_ENV',
+    selectedEnvironments: data.selectedEnvironments,
     chartsToDeploy: data.chartsToDeploy
   }
 });
